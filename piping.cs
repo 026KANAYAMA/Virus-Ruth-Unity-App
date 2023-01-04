40d32aa86e20c8dc44c907a0c8fb36f239085b1e
KANAYAMA <103913975+026KANAYAMA@users.noreply.github.com>
2023-01-04 19:33:22 +0900

    except ip address, almost complete

--- piping.cs
+++ piping.cs
@@ -1,13 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using System;
+using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
-using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
 
 public class piping
 {
-    public string firstName;
-    
+    private static readonly HttpClient httpClient = new HttpClient();
+
+
+
+    public async static Task<string> htpAsync(string player)
+    {
+         //piping serverから受けとったデータを格納したい
+        string key = "0";  // http://localhost:8080/key の形でpiping-serverにアクセスしたい
+        
+        if(player == "Virus"){
+            key = "Virus";
+        }
+        else if(player == "People"){
+            key = "People";
+        }
+        
+        //await numDirect = 
+        var numDirect = await Getman();
+        
+        Task<string> Getman(){
+            Task<string> num  = httpClient.GetStringAsync($"http://192.168.0.5:8888/{key}");
+            //Debug.Log(num);
+            return num;
+        }
+
+        return numDirect;
+    }
+        
+        
 }
+
