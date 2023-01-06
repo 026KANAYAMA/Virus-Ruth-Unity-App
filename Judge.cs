524c8d8faba8c40334dc0b9028007a2e8f425099
KANAYAMA <103913975+026KANAYAMA@users.noreply.github.com>
2023-01-07 01:40:05 +0900

    v.1.0(before voice recognition)

---
+++ Judge.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.SceneManagement;
+
+public class Judge
+{
+    public static void excute()
+    {
+        Vector3 virus = Entry.vPos.position;
+        Vector3 people = Entry.pPos.position;
+
+        float x_abs = Math.Abs(virus.x - people.x);
+        float y_abs = Math.Abs(virus.y - virus.y);
+
+        if(x_abs <= 1 && y_abs <= 1){
+            SceneManager.LoadScene("VirusWinScenes");
+        }
+
+    }
+
+
+}
