ff37b91bedc65785ff8917db12f9dff10652b65d
KANAYAMA <103913975+026KANAYAMA@users.noreply.github.com>
2023-01-07 01:33:34 +0900

    v.1.0(before voice recognition)

--- InputField.cs
+++ InputField.cs
@@ -6,23 +6,17 @@ using UnityEngine.SceneManagement;
 
 public class InputField : MonoBehaviour
 {
-    //InputFieldを格納するための変数
-    //InputField inputField;
 
     public static string ip;
 
-    void Start()
-    {
-        //InputFieldコンポーネントを取得
-        
-    }
+    
 
     public void GetIP(){
         Text FieldText = GameObject.Find("InputField/Text").GetComponent<Text>();
         //InputFieldからテキスト情報を取得する
         ip = FieldText.text;
         Debug.Log(ip);
-        SceneManager.LoadScene("GameScenes");
+        SceneManager.LoadScene("DescriptionScenes");
     }
     
 }
